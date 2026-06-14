4527f1e349aad9801dbe5c2468a83c16814f4aaf
JayanAXHF <sunil.chdry@gmail.com>
2026-06-14 22:05:43 +0530

    fix(config): add some logging to make my life easier

--- src/config/file.rs
+++ src/config/file.rs
@@ -2,6 +2,7 @@ use std::{fs, path::Path};
 
 use serde::Deserialize;
 use snafu::ResultExt;
+use tracing::info;
 
 use crate::error::{AppError, AppResult, ConfigParseSnafu, ConfigReadSnafu};
 
@@ -89,6 +90,8 @@ impl BootstrapConfig {
                 path: banlist_path.to_path_buf(),
             })?;
         config.banlist = banlist;
+        info!(banned_ids = ?config.banlist.ids.len(), "Loaded ban list with ");
+        info!(pingable_members = ?config.join_pinglist.members.len(), "Loaded pinglist with");
         config.validate()?;
         Ok(config)
     }
